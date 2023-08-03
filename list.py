a6e843e34ecf5ff1af759ff2b98d4aaed585db45
Aditi-223 <141294835+Aditi-223@users.noreply.github.com>
2023-08-03 09:37:44 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+u_input=input("Enter list of numbers: ")
+list1=[int(n) for n in u_input.split()]
+max_list=max(list1)
+print("Maximum number in list: ",max_list)
+
+
+# In[ ]:
+
+
+
+
