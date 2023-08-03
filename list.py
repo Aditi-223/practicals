90bc5ae86a2f3851a1f6049acb814674d6b41f0d
Aditi-223 <141294835+Aditi-223@users.noreply.github.com>
2023-08-03 10:10:15 +0530

    Update list.py

--- list.py
+++ list.py
@@ -7,8 +7,9 @@
 u_input=input("Enter list of numbers: ")
 list1=[int(n) for n in u_input.split()]
 max_list=max(list1)
+min_list=min(list1)
 print("Maximum number in list: ",max_list)
-
+print("Minimum number in list: ",min_list)
 
 # In[ ]:
 
